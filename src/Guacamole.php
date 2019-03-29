813833815704348238ef55f8a90e57c3f255c912
omizzy <1174759+omizzy@users.noreply.github.com>
2019-03-29 13:23:51 -0700

    start a recipe

---
+++ src/Guacamole.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Recipes\Avocado;
+
+class Guacamole
+{
+    /**
+     * @var int
+     */
+    private $howMuch;    
+    public function __construct($howMuch)
+    {
+        // set it
+        $this->howMuch = $howMuch
+    }
+}
