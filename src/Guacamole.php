630b833a79859865628960b7925249a830e541b3
omizzy <1174759+omizzy@users.noreply.github.com>
2019-03-29 13:30:04 -0700

    add the recipe

--- src/Guacamole.php
+++ src/Guacamole.php
@@ -2,12 +2,24 @@
 
 namespace Recipes\Avocado;
 
+/**
+ * Class Guacamole
+ */
 class Guacamole
 {
     /**
      * @var int
      */
     private $howMuch;
+    
+    private $text=<<<RECIPE
+To make %s ounces of guacamole, you will need
+1) Avocados - smash them up
+2) Onions - cut them up into bits
+3) Tomatos - cut them up
+4) Lemon - squeeze out the juice 
+5) Blend all of these
+RECIPE;
     
     /**
      * Constructor
@@ -16,5 +28,10 @@ class Guacamole
     {
         // set it
         $this->howMuch = $howMuch
+    }
+
+    public function ingredients()
+    {
+        return sprintf(self::$text, $this->howMuch);
     }
 }
